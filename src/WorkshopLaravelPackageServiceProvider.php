05eb5a3e42a8ee289c04f940bbc07aee1a7c2047
freekmurze <freekmurze@users.noreply.github.com>
2021-01-20 12:40:57 +0000

    Fix styling

--- src/WorkshopLaravelPackageServiceProvider.php
+++ src/WorkshopLaravelPackageServiceProvider.php
@@ -10,22 +10,22 @@ class WorkshopLaravelPackageServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-        /*
-            $this->publishes([
-                __DIR__ . '/../config/workshop-laravel-package.php' => config_path('workshop-laravel-package.php'),
-            ], 'config');
-
-            $this->publishes([
-                __DIR__ . '/../resources/views' => base_path('resources/views/vendor/workshop-laravel-package'),
-            ], 'views');
-
-            $migrationFileName = 'create_workshop_laravel_package_table.php';
-            if (! $this->migrationFileExists($migrationFileName)) {
+            /*
                 $this->publishes([
-                    __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
-                ], 'migrations');
-            }
-        */
+                    __DIR__ . '/../config/workshop-laravel-package.php' => config_path('workshop-laravel-package.php'),
+                ], 'config');
+
+                $this->publishes([
+                    __DIR__ . '/../resources/views' => base_path('resources/views/vendor/workshop-laravel-package'),
+                ], 'views');
+
+                $migrationFileName = 'create_workshop_laravel_package_table.php';
+                if (! $this->migrationFileExists($migrationFileName)) {
+                    $this->publishes([
+                        __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
+                    ], 'migrations');
+                }
+            */
 
             $this->commands([
                 ShowTextCommand::class,
@@ -37,7 +37,7 @@ class WorkshopLaravelPackageServiceProvider extends ServiceProvider
 
     public function register()
     {
-     //   $this->mergeConfigFrom(__DIR__ . '/../config/workshop-laravel-package.php', 'workshop-laravel-package');
+        //   $this->mergeConfigFrom(__DIR__ . '/../config/workshop-laravel-package.php', 'workshop-laravel-package');
     }
 
     /*
